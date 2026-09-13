f70cac932e59b910e494d7c577710b95bfc5d602
kennychino <kenny_27_28@hotmail.com>
2026-09-13 17:43:24 +0000

    complete timestamp microservice

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -13,9 +13,33 @@ app.get("/", (_req, res) => {
 
 // Do not change code above this line
 
+app.get("/api{/:date}", (req, res) => {
+  const dateParam = req.params.date;
+
+  let date;
+
+  if (!dateParam) {
+    date = new Date();
+  } else if (/^\d+$/.test(dateParam)) {
+    date = new Date(Number(dateParam));
+  } else {
+    date = new Date(dateParam);
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
 // Do not change code below this line
 
 const PORT = 8000;
+
 const listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
